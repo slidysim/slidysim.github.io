9eb14a45274f561ebd6e9982310951ef00291844
dphdmn <pwfan1000@gmail.com>
2025-06-15 14:13:01 +0800

    v3.6.1 - FMC power new reqs

--- power/js/populatetable.js
+++ power/js/populatetable.js
@@ -1,5 +1,5 @@
 
-import {FMCtiers, tiersNew, categoriesNew, tiersOld, categoriesOld, categoriesFMC, data } from "./data.js?v=1.0.121";
+import {FMCtiers, tiersNew, categoriesNew, tiersOld, categoriesOld, categoriesFMC, data } from "./data.js?v=1.0.122";
 
 let powerData;
 let tiers;
