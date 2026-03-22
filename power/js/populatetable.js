13f70a34525d2c12cf423f587a2ba0c5f726a43f
dphdmn <pwfan1000@gmail.com>
2026-03-23 07:14:19 +0800

    v4.2.0 - g++ rework

--- power/js/populatetable.js
+++ power/js/populatetable.js
@@ -1,5 +1,5 @@
 
-import {FMCtiers, tiersNew, categoriesNew, tiersOld, categoriesOld, categoriesFMC, data } from "./data.js?v=1.0.124";
+import {FMCtiers, tiersNew, categoriesNew, tiersOld, categoriesOld, categoriesFMC, data } from "./data.js?v=1.0.126";
 
 let powerData;
 let tiers;
